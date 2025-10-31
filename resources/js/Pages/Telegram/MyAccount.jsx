70c492ba3023613f958662c30b25465673de62ef
MahdiRezaeiDev <mahdi.sohaily4030@gmail.com>
2025-10-31 10:45:58 +0430

    Added disconnect option to the telegram account info

--- resources/js/Pages/Telegram/MyAccount.jsx
+++ resources/js/Pages/Telegram/MyAccount.jsx
@@ -93,12 +93,6 @@ export default function MyAccount() {
                                 >
                                     🔄 ثبت مجدد حساب
                                 </button>
-                                <button
-                                    onClick={handleDisconnect}
-                                    className="rounded-lg bg-red-500 px-4 py-2 text-sm font-medium text-white transition hover:bg-red-600"
-                                >
-                                    ❌ قطع اتصال
-                                </button>
                             </div>
                         </div>
                     ) : (
